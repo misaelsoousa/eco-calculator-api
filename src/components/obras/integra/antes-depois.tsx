0521b0a6de7cd46885cc12ead7100f88087fedaf
misaelsoousa <misasoousa@gmail.com>
2025-07-15 07:35:35 -0300

    retirado src

--- src/components/obras/integra/antes-depois.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const ReactCompareImage = dynamic(() => import("react-compare-image"), { ssr: false });
-import { ArrowSlider } from "../../../../public/img/svg/icons"; 
